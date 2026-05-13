0e61fb479784a2d96f121162cdae1e80a9239599
Saraiva18k <pedrohenriquecoelhosaraiva2@hotmail.com>
2026-05-13 18:38:10 -0300

    refactor(precificacao.tsx): replace TopBar with ContextTabs floating inside scroll

--- src/routes/precificacao.tsx
+++ src/routes/precificacao.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/mios/Header";
 import { Sidebar } from "@/components/mios/Sidebar";
-import { TopBar } from "@/components/mios/TopBar";
 import { Balanca } from "@/components/mios/precificacao/Balanca";
+import { ContextTabs } from "@/components/mios/ContextTabs";
 
 export const Route = createFileRoute("/precificacao")({
   component: PrecificacaoPage,
@@ -31,8 +31,8 @@ function PrecificacaoPage() {
       <div className="flex flex-1 min-h-0">
         <Sidebar activeModule="Precificação" />
         <main className="flex-1 flex flex-col min-w-0">
-          <TopBar />
           <div className="mios-scroll flex-1 overflow-y-auto px-6 py-5">
+            <ContextTabs />
             <Balanca />
           </div>
         </main>
