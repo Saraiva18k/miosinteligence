bfb15f64449430e4e0dab010eb8ae211915de386
Saraiva18k <pedrohenriquecoelhosaraiva2@hotmail.com>
2026-05-13 18:38:14 -0300

    refactor(veredito.tsx): replace TopBar with ContextTabs floating inside scroll

--- src/routes/veredito.tsx
+++ src/routes/veredito.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/mios/Header";
 import { Sidebar } from "@/components/mios/Sidebar";
-import { TopBar } from "@/components/mios/TopBar";
 import { VerdictHero } from "@/components/mios/VerdictHero";
+import { ContextTabs } from "@/components/mios/ContextTabs";
 
 export const Route = createFileRoute("/veredito")({
   component: VereditorPage,
@@ -26,8 +26,8 @@ function VereditorPage() {
       <div className="flex flex-1 min-h-0">
         <Sidebar activeModule="Veredito" />
         <main className="flex-1 flex flex-col min-w-0">
-          <TopBar />
           <div className="mios-scroll flex-1 overflow-y-auto px-6 py-5">
+            <ContextTabs />
             <VerdictHero />
           </div>
         </main>
