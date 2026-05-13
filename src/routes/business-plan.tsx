86e739de36664cb9f009167a30a2455f631c541a
Saraiva18k <pedrohenriquecoelhosaraiva2@hotmail.com>
2026-05-13 18:37:59 -0300

    refactor(business-plan.tsx): replace TopBar with ContextTabs floating inside scroll

--- src/routes/business-plan.tsx
+++ src/routes/business-plan.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/mios/Header";
 import { Sidebar } from "@/components/mios/Sidebar";
-import { TopBar } from "@/components/mios/TopBar";
 import { Manuscript } from "@/components/mios/businessplan/Manuscript";
+import { ContextTabs } from "@/components/mios/ContextTabs";
 
 export const Route = createFileRoute("/business-plan")({
   component: BusinessPlanPage,
@@ -30,7 +30,6 @@ function BusinessPlanPage() {
       <div className="flex flex-1 min-h-0">
         <Sidebar activeModule="Business Plan" />
         <main className="flex-1 flex flex-col min-w-0">
-          <TopBar />
           <div className="flex-1 overflow-hidden px-6 py-5">
             <Manuscript />
           </div>
