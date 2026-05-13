9b0a87f85eb2f8267e1c15c34d78000bb4b2e219
Saraiva18k <pedrohenriquecoelhosaraiva2@hotmail.com>
2026-05-13 18:38:11 -0300

    refactor(sentimento.tsx): replace TopBar with ContextTabs floating inside scroll

--- src/routes/sentimento.tsx
+++ src/routes/sentimento.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/mios/Header";
 import { Sidebar } from "@/components/mios/Sidebar";
-import { TopBar } from "@/components/mios/TopBar";
 import { Pulso } from "@/components/mios/sentimento/Pulso";
+import { ContextTabs } from "@/components/mios/ContextTabs";
 
 export const Route = createFileRoute("/sentimento")({
   component: SentimentoPage,
@@ -31,8 +31,8 @@ function SentimentoPage() {
       <div className="flex flex-1 min-h-0">
         <Sidebar activeModule="Sentimento" />
         <main className="flex-1 flex flex-col min-w-0">
-          <TopBar />
           <div className="mios-scroll flex-1 overflow-y-auto px-6 py-5">
+            <ContextTabs />
             <Pulso />
           </div>
         </main>
