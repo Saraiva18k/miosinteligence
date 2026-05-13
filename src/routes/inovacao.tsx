47f30ce7d68f86a6a0b140e67bfb699d9da9bec4
Saraiva18k <pedrohenriquecoelhosaraiva2@hotmail.com>
2026-05-13 17:40:38 -0300

    fix(routes): add px-6 py-5 padding to inovacao route

--- src/routes/inovacao.tsx
+++ src/routes/inovacao.tsx
@@ -32,7 +32,7 @@ function InovacaoPage() {
         <Sidebar activeModule="Inovação" />
         <main className="flex-1 flex flex-col min-w-0">
           <TopBar />
-          <div className="mios-scroll flex-1 overflow-y-auto">
+          <div className="mios-scroll flex-1 overflow-y-auto px-6 py-5">
             <Laboratorio />
           </div>
         </main>
